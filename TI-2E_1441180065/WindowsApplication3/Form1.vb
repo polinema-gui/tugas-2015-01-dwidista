ee8313ef2ed7b195c3d6d9e7c7bcfccdc1c8b120
DwiDista <dnsdista@gmail.com>
2015-10-06 07:43:33 +0700

    tugas1

---
+++ TI-2E_1441180065/WindowsApplication3/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Black Then
+            Me.BackColor = Color.Red
+        ElseIf Me.BackColor = Color.Red Then
+            Me.BackColor = Color.Black
+        End If
+    End Sub
+End Class
